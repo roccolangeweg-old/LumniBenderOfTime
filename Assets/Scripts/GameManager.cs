225034f9da200d51a8537f8873c3301c3776bdbb
Rocco Langweg <rocco.langeweg@gmail.com>
2015-09-09 00:07:42 +0200

    Cleanup & Main Menu

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance = null;
+
+    //Awake is always called before any Start functions
+    void Awake() {
+        /* check if gamemanager already exists */
+        if (instance == null) {
+            instance = this;
+        } else if (instance != this) {
+            /* enforce Singleton, only 1 game manager may exist */
+            Destroy(gameObject);
+        }    
+            
+        //Sets this to not be destroyed when reloading scene
+        DontDestroyOnLoad(gameObject);
+    }
+           
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        if (Application.loadedLevelName == "MainMenu" && Input.GetMouseButtonDown(0)) {
+            Application.LoadLevel("Gamescreen");
+        }
+	}
+}
--- Assets/Scripts/Gamescripts/AttackController.cs
+++ Assets/Scripts/Gamescripts/AttackController.cs
@@ -13,7 +13,7 @@ public class AttackController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         if (myAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1 && !myAnimator.IsInTransition(0)) {
-            gameObject.SetActive(false);
+            Destroy(this.gameObject);
         }
 	}
 }
