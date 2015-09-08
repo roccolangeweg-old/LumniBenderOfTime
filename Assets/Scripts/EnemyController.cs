916f3f467b7fc422921fb122568ca31d350f702a
Rocco Langweg <rocco.langeweg@gmail.com>
2015-09-08 15:58:18 +0200

    Enemy fly away after death

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -8,6 +8,8 @@ public class EnemyController : MonoBehaviour {
     public bool isAerialType;
     public float moveSpeed;
 
+    private bool isAlive;
+
     private Vector3 basePosition;
     public float flyingSwing;
 
@@ -24,6 +26,7 @@ public class EnemyController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         currentHealth = baseHealth;
+        isAlive = true;
         myRigidbody = this.GetComponent<Rigidbody2D>();
         myAnimator = this.GetComponent<Animator>();
         basePosition = transform.position;
@@ -32,36 +35,49 @@ public class EnemyController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        knockbackTime -= Time.deltaTime;
+        if (isAlive) {
+        
+            knockbackTime -= Time.deltaTime;
 
-        if (!isKnockedBack) {
-            myRigidbody.velocity = new Vector2(-moveSpeed, myRigidbody.velocity.y);
-        } else if (isKnockedBack && knockbackTime <= 0) {      
-            isKnockedBack = false;
-        }
-
-        if (isAerialType) {
-
-            if(transform.position.y < basePosition.y - flyingSwing) {
-                myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, flyingSwing * 3);
+            if (!isKnockedBack) {
+                myRigidbody.velocity = new Vector2(-moveSpeed, myRigidbody.velocity.y);
+            } else if (isKnockedBack && knockbackTime <= 0) {      
+                isKnockedBack = false;
             }
 
+            if (isAerialType) {
+
+                if (transform.position.y < basePosition.y - flyingSwing) {
+                    myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, flyingSwing * 3);
+                }
+
+            }
+
+            if (currentHealth <= 0) {
+                myAnimator.SetBool("Alive", false);
+                myRigidbody.constraints = RigidbodyConstraints2D.None;
+                isAlive = false;
+                // this.GetComponent<Collider2D>().isTrigger = true;
+                int shootUp = Random.Range(0, 1);
+                if (shootUp == 1) {
+                    this.myRigidbody.velocity = new Vector2(10, 3);      
+                } else {
+                    this.myRigidbody.velocity = new Vector2(10, -3);
+                }
+
+            }
+            
+            myAnimator.SetFloat("Speed", Mathf.Abs(myRigidbody.velocity.x));
+            myAnimator.SetBool("Knockback", isKnockedBack);
         }
 
-        myAnimator.SetFloat("Speed", Mathf.Abs(myRigidbody.velocity.x));
-        myAnimator.SetBool("Knockback", isKnockedBack);
 
-        if (currentHealth < 0) {
-            myAnimator.SetBool("Alive", false);
-            this.GetComponent<Collider2D>().isTrigger = false;
-            this.myRigidbody.velocity = new Vector2(0,0);
-        }
 
 	}
 
     void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.tag == "Attack") {
-            //currentHealth-=1;
+            currentHealth-=1;
 
             isKnockedBack = true;
             knockbackTime = knockbackLength;
