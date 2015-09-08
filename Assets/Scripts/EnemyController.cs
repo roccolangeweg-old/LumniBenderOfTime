ea6ffa2cd4d9f32606f32c326350372e6c6c525d
Rocco Langweg <rocco.langeweg@gmail.com>
2015-09-08 14:26:40 +0200

    enemy knockback

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -9,6 +9,11 @@ public class EnemyController : MonoBehaviour {
     public float moveSpeed;
 
     private int currentHealth;
+    private bool isKnockedBack;
+
+    private float knockbackTime;
+    public float knockbackLength;
+    public float knockbackAmplifier;
 
     private Rigidbody2D myRigidbody;
     private Animator myAnimator;
@@ -22,7 +27,15 @@ public class EnemyController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        myRigidbody.velocity = new Vector2(-moveSpeed, myRigidbody.velocity.y);
+
+        knockbackTime -= Time.deltaTime;
+
+        if (!isKnockedBack) {
+            myRigidbody.velocity = new Vector2(-moveSpeed, myRigidbody.velocity.y);
+        } else if (isKnockedBack && knockbackTime <= 0) {      
+            isKnockedBack = false;
+        }
+
         myAnimator.SetFloat("Speed", Mathf.Abs(myRigidbody.velocity.x));
 
         if (currentHealth < 0) {
@@ -30,11 +43,17 @@ public class EnemyController : MonoBehaviour {
             this.GetComponent<Collider2D>().isTrigger = false;
             this.myRigidbody.velocity = new Vector2(0,0);
         }
+
+        Debug.Log(myRigidbody.velocity.x);
 	}
 
     void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.tag == "Attack") {
-            currentHealth-=1;
+            //currentHealth-=1;
+
+            isKnockedBack = true;
+            knockbackTime = knockbackLength;
+            myRigidbody.velocity = new Vector2(3 * knockbackAmplifier * 1f, 5 * knockbackAmplifier * 0.3f);
         }
     }
 }
