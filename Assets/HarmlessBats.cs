cb3ecc75192d95dad5eb7880e402e154ce4850e7
Rocco Langweg <rocco.langeweg@gmail.com>
2015-09-09 15:29:04 +0200

    sound on hit + bat cave in scene

---
+++ Assets/HarmlessBats.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HarmlessBats : MonoBehaviour {
+
+    private float upwardDirection;
+
+	// Use this for initialization
+	void Start () {
+        upwardDirection = Random.Range(3f,5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(5, upwardDirection);
+	}
+}
