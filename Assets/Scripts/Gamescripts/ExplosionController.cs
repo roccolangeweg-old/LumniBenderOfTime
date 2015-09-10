466bcad027c5d2d61aa62b9ced41a101467bb375
Rocco Langweg <rocco.langeweg@gmail.com>
2015-09-10 23:32:58 +0200

    explosion bug (finally fixed)

--- Assets/Scripts/Gamescripts/ExplosionController.cs
+++ Assets/Scripts/Gamescripts/ExplosionController.cs
@@ -8,11 +8,13 @@ public class ExplosionController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         myAnimator = GetComponent<Animator>();
+       // myAnimator.SetBool("Aerial", false);
+       // myAnimator.SetBool("Ground", false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (myAnimator.GetCurrentAnimatorStateInfo(0).IsName("Explosion_Ground") || myAnimator.GetCurrentAnimatorStateInfo(0).IsName("Explosion_Air")) {
+        if (myAnimator.GetCurrentAnimatorStateInfo(0).IsName("Explosion_Ground") || myAnimator.GetCurrentAnimatorStateInfo(0).IsName("Explosion_Aerial")) {
 
             if (myAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1 && !myAnimator.IsInTransition(0)) {
                 Destroy(gameObject);
